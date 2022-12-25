3bb989ef928248468612f4a398b85cfb0e4a48d2
josephfernicola <josephrfernicola@gmail.com>
2022-12-24 20:15:03 -0800

    directions reduction instruction

--- Solutions/directionsReduction.js
+++ Solutions/directionsReduction.js
@@ -1,3 +1,5 @@
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
 function dirReduc(arr){
     const result = []
         for(let i = 0; i < arr.length; i++) {

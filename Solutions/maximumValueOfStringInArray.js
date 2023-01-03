ee532bbbf1dff442c996a09a21bacb7c77b29cf7
josephfernicola <josephrfernicola@gmail.com>
2023-01-02 21:08:51 -0800

    maximum value solution. Passed one test case

--- Solutions/maximumValueOfStringInArray.js
+++ Solutions/maximumValueOfStringInArray.js
@@ -4,3 +4,15 @@
 // The length of the string, otherwise.
 // Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
 
+var maximumValue = function(strs) {
+    let output = 0;
+    for (let i = 0; i < strs.length; i++) {
+        if (parseInt(strs[i]) !== NaN) {
+            output = Math.max(parseInt(strs[i]), output);
+        }
+        else {
+            output = Math.max(strs[i].length, output);
+        }
+    }
+    return output;
+};

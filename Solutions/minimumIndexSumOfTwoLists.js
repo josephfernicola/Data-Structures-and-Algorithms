22c5446f05807f00a52619da7431f9994f51106e
josephfernicola <josephrfernicola@gmail.com>
2023-01-05 20:39:42 -0800

    minimum index sum of two lists solution

--- Solutions/minimumIndexSumOfTwoLists.js
+++ Solutions/minimumIndexSumOfTwoLists.js
@@ -6,3 +6,23 @@
 
 // Return all the common strings with the least index sum. Return the answer in any order.
 
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+ const findRestaurant = function(list1, list2) {
+    const map = {}
+    for (let i = 0; i < list1.length; i++) {
+        const list1Word = list1[i];
+        const index = list2.indexOf(list1Word);
+        if(index > -1) {
+            if (map[index + i]) {
+                map[index + i].push(list1Word);
+            } else {
+                map[index + i] = [list1Word];
+            }
+        }
+    }
+    return Object.values(map)[0]
+};

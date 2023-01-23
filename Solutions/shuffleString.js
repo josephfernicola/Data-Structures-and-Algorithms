1f35f9a4ea79c9bb66aa51c2823840d0aa28c8bd
josephfernicola <josephrfernicola@gmail.com>
2023-01-23 08:53:30 -0800

    directions for shuffle string

--- Solutions/shuffleString.js
+++ Solutions/shuffleString.js
@@ -1,3 +1,8 @@
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+
 /**
  * @param {string} s
  * @param {number[]} indices
